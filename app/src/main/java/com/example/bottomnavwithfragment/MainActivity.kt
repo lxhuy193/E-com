b10dae5dee05324c2e375a0d4ccf831dc8162801
kubomnhata <55189177+kubomnhata@users.noreply.github.com>
2022-03-03 16:23:57 +0700

    3/3 BottomNav + Fragment

---
+++ app/src/main/java/com/example/bottomnavwithfragment/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.bottomnavwithfragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavView)
+        val navController = findNavController(R.id.fragmentContainerView)
+
+        bottomNavigationView.setupWithNavController(navController)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.dashbroadFragment,
+                R.id.searchFragment,
+                R.id.cartFragment,
+                R.id.profileFragment
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+}
